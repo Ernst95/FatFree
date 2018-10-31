38d4f22615508c664382dabf8cd12dd1ed929336
Ernst95 <ErnstC@AWARDS.CO.ZA>
2018-10-31 16:01:30 +0200

    added database functionality

--- App/Controllers/Contact.php
+++ App/Controllers/Contact.php
@@ -1,8 +1,25 @@
 <?php
 
 class Contact {
+    
     function index() {
         echo 'Hello world from contact';
+    }
+
+    function getAllUsers() {
+        
+        $db = new DB\SQL (
+            'mysql:host=localhost;port=3306;dbname=salon',
+            'root',
+            ''
+        );
+
+        $result = $db->exec(
+            array('SELECT * FROM user')
+        );
+
+        var_dump($result);
+
     }
 }
 
