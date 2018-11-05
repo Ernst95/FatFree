b19919c2002fcad617578417e509a734112f97e2
Ernst95 <ErnstC@AWARDS.CO.ZA>
2018-11-05 16:51:25 +0200

    code cleanup

--- App/Controllers/Contact.php
+++ App/Controllers/Contact.php
@@ -6,21 +6,6 @@ class Contact extends Controller{
         echo 'Hello world from contact';
     }
 
-    function getAllUsers() {
-        
-        $db = new DB\SQL (
-            'mysql:host=localhost;port=3306;dbname=salon',
-            'root',
-            ''
-        );
-
-        $result = $db->exec(
-            array('SELECT * FROM user')
-        );
-
-        var_dump($result);
-
-    }
 }
 
 ?>
