e39d3af5a15d2c431042c55fcb58ff9216eb2660
Ernst95 <ErnstC@AWARDS.CO.ZA>
2018-11-05 16:52:34 +0200

    added database functionality

--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -2,12 +2,30 @@
 
 class Controller {
 
+    protected $f3;
+    protected $db;
+
     function beforeroute() {
-        echo 'Before route - ';
+        //echo 'Before route - ';
     }
 
     function afterroute() {
-        echo '- After route';
+        //echo '- After route';
+    }
+
+    function __construct() {
+        
+        $f3 = Base::instance();
+        $this->f3 = $f3;
+
+        $db = new DB\SQL(
+            'mysql:host=localhost;port=3306;dbname=salon',
+            'root',
+            '',
+            array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION)
+        );
+
+        $this->db = $db;
     }
     
 }
