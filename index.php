9fef11f3474085ac67d846a2cde16aa259ee686c
Ernst95 <ErnstC@AWARDS.CO.ZA>
2018-11-05 16:50:21 +0200

    changed to new config file names

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require_once('vendor/autoload.php');
  
 $f3 = Base::instance();
  
-$f3->config('App/Config/setup.cfg');
+$f3->config('App/Config/setup.ini');
 
-$f3->config('App/Config/routes.cfg');
+$f3->config('App/Config/routes.ini');
  
 $f3->run();
 
