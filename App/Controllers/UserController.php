907a1300d26a7c81c93912b71ab6938c9519c1d2
Ernst95 <ErnstC@AWARDS.CO.ZA>
2018-11-06 17:08:15 +0200

    added crud functionality

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -33,11 +33,14 @@ class UserController extends Controller {
         $userId = $params['userId'];
 
         if(empty($userId)) {
+
             echo json_encode(array(
                 'success' => false,
-                'message' => 'Missing fields'
+                'message' => 'Missing one or more required fields'
             ));
+
             return;
+
         }
 
         $user = new User($this->db);
@@ -48,6 +51,97 @@ class UserController extends Controller {
             'success' => true,
             'count' => count($result),
             'results' => $result
+        ));
+
+    }
+
+    function create($f3, $params) {
+
+        header('Content-type:application/json');
+
+        $data = json_decode($f3->get('BODY'), true);
+
+        if(!empty($params['userId'])) {
+
+            $data['userId'] = $userId;
+
+            $user = new User($this->db);
+    
+            $result = $user->getById($userId);
+
+            if(count($result) <= 0) {
+            
+                echo json_encode(array(
+                    'success' => false,
+                    'message' => 'User does not exist'
+                ));
+    
+                return;
+    
+            }
+
+            $user->create($data);
+
+            echo json_encode(array(
+                'success' => true,
+                'message' => 'User successfully updated'
+            ));
+
+        }
+        else {
+
+            $user->create($data);
+
+            echo json_encode(array(
+                'success' => true,
+                'message' => 'User successfully created'
+            ));
+
+        }
+        
+    }
+
+    function delete($f3, $params) {
+
+        header('Content-type:application/json');
+
+        $userId = $params['userId'];
+
+        if(empty($userId)) {
+
+            echo json_encode(array(
+                'success' => false,
+                'message' => 'Missing one or more required fields'
+            ));
+            
+            return;
+        
+        }
+
+        $user = new User($this->db);
+
+        $result = $user->getById($userId);
+
+        if(count($result) <= 0) {
+
+            echo json_encode(array(
+                'success' => false,
+                'message' => 'User does not exist'
+            ));
+
+            return;
+
+        }
+
+        $data['modified'] = date('Y-m-d H:i:s');
+        $data['userId'] = $userId;
+        $data['disabled'] = 1;
+
+        $user->delete($data);
+
+        echo json_encode(array(
+            'success' => true,
+            'message' => 'User successfully deactivated'
         ));
 
     }
