6480f6f045336ffbcd3a6fc44547d8c0841bbc34
Ernst95 <ErnstC@AWARDS.CO.ZA>
2018-11-20 15:37:58 +0200

    added environment variable

--- index.php
+++ index.php
@@ -5,10 +5,26 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 date_default_timezone_set("Africa/Johannesburg");
- 
-$f3->config('App/Config/setup.ini');
 
-$f3->config('App/Config/routes.ini');
+$f3->config('App/Config/routes/routes.ini');
+
+$f3->config('App/Config/environment.ini');
+
+if($f3->get('env') == "dev") {
+
+    $f3->config('App/Config/dev/setup.ini');
+
+    $f3->config('App/Config/dev/mysql.ini');
+
+}
+
+if($f3->get('env') == "prod"){
+
+    $f3->config('App/Config/prod/setup.ini');
+
+    $f3->config('App/Config/prod/mysql.ini');
+
+}
  
 $f3->run();
 
